cceb6d8460396524dfd385c0a0f1dcb0dcccf952
Parham Alvani <parham.alvani@gmail.com>
2020-07-01 09:14:47 +0430

    feat: Test Connz in Main

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,5 +1,10 @@
 mod nats;
 
-fn main() {
-    println!("Hello, world!");
+use nats::client;
+
+#[tokio::main]
+async fn main() {
+    let client = client::Client::new("http://127.0.0.1:8222");
+    let connz = client.connz(0, 0).await.expect("fetching connz failed");
+    println!("{:?}", connz);
 }
