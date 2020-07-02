1bc9d09cfaf15aed68644680aaa60357ef2ea863
Parham Alvani <parham.alvani@gmail.com>
2020-07-02 14:35:43 +0430

    feat: Redesign Using Ants Design

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,10 +7,18 @@ import { AppComponent } from './app.component';
 import { NATSService } from './nats/nats.service';
 
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
-import { NbIconModule, NbThemeModule, NbLayoutModule, NbCardModule } from '@nebular/theme';
-import { NbEvaIconsModule } from '@nebular/eva-icons';
 import { ServerComponent } from './server/server.component';
 import { ServersComponent } from './servers/servers.component';
+
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { NzDescriptionsModule } from 'ng-zorro-antd/descriptions';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzSpaceModule } from 'ng-zorro-antd/space';
+import { NzCardModule } from 'ng-zorro-antd/card';
+import { NzPageHeaderModule } from 'ng-zorro-antd/page-header';
+import { NzStatisticModule } from 'ng-zorro-antd/statistic';
+
 
 @NgModule({
   declarations: [
@@ -23,14 +31,17 @@ import { ServersComponent } from './servers/servers.component';
     AppRoutingModule,
     HttpClientModule,
     NoopAnimationsModule,
-    NbCardModule,
-    NbIconModule,
-    NbThemeModule.forRoot({ name: 'dark' }),
-    NbLayoutModule,
-    NbEvaIconsModule
+    NzLayoutModule,
+    NzDescriptionsModule,
+    NzGridModule,
+    NzSpaceModule,
+    NzCardModule,
+    NzPageHeaderModule,
+    NzMenuModule,
+    NzStatisticModule
   ],
   providers: [
-    NATSService
+    NATSService,
   ],
   bootstrap: [AppComponent]
 })
--- frontend/src/app/server/server.component.ts
+++ frontend/src/app/server/server.component.ts
@@ -1,20 +1,22 @@
 import { Component, OnInit, Input } from '@angular/core';
-import { NATSService, Connz } from '../nats/nats.service';
+import { NATSService, Connz, Varz } from '../nats/nats.service';
 
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styleUrls: ['./server.component.scss']
+  styleUrls: ['./server.component.less']
 })
 export class ServerComponent implements OnInit {
 
   connz: Connz;
+  varz: Varz;
   @Input() name: string;
 
   constructor(private natsService: NATSService) {  }
 
   ngOnInit(): void {
     this.natsService.connz(this.name).subscribe(connz => this.connz = connz);
+    this.natsService.varz(this.name).subscribe(varz => this.varz = varz);
   }
 
 }
