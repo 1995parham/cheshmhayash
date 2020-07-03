2aa9d739bf48864a482bc7cc24d58d0d1b3ab056
Parham Alvani <parham.alvani@gmail.com>
2020-07-03 12:28:16 +0430

    feat: Remove Redundant Depenedcies

--- backend/src/nats/client.rs
+++ backend/src/nats/client.rs
@@ -1,6 +1,8 @@
 use super::connz::Connz;
 use super::varz::Varz;
 
+use actix_web::client;
+
 #[derive(Clone)]
 pub struct Client {
     url: String,
@@ -13,18 +15,18 @@ impl Client {
         }
     }
 
-    pub async fn connz(&self, offset: u64, limit: u64) -> Result<Connz, reqwest::Error> {
-        let client = reqwest::Client::new();
+    pub async fn connz(&self, offset: u64, limit: u64) -> Result<Connz, actix_web::Error> {
+        let client = client::Client::new();
 
         let connz = client
             .get(format!("{}/connz", self.url).as_str())
-            .query(&[("offset", offset), ("limit", limit), ("subs", 1)])
+            .query(&[("offset", offset), ("limit", limit), ("subs", 1)])?
             .send().await?.json::<Connz>().await?;
         Ok(connz)
     }
 
-    pub async fn varz(&self) -> Result<Varz, reqwest::Error> {
-        let client = reqwest::Client::new();
+    pub async fn varz(&self) -> Result<Varz, actix_web::Error> {
+        let client = client::Client::new();
 
         let connz = client
             .get(format!("{}/varz", self.url).as_str())
