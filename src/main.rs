ef42d0f6ce8b346ab203391877b6adb3a79f1aee
Parham Alvani <1995parham@tuta.io>
2021-08-29 02:48:34 +0430

    feat: update main

--- src/main.rs
+++ src/main.rs
@@ -9,51 +9,55 @@ use std::collections::HashMap;
 use setting::Settings;
 
 async fn index() -> Result<fs::NamedFile> {
-    Ok(fs::NamedFile::open(
-        "../frontend/dist/cheshmhayash/index.html",
-    )?)
+    Ok(fs::NamedFile::open("web/dist/cheshmhayash/index.html")?)
 }
 
 #[actix_web::main]
 async fn main() {
     let setting = Settings::new().expect("loading configuration failed");
     println!("settings: {:?}", setting);
-    let setting_clone = setting.clone();
 
     println!(
         "listen on {}:{}",
         setting.server().host(),
         setting.server().port()
     );
-    HttpServer::new(move || {
-        let clients: HashMap<String, nats::Client> = setting_clone
-            .nats()
-            .iter()
-            .map(|s| (s.name().to_string(), nats::Client::new(s.monitoring())))
-            .collect();
 
-        let nats_handler = handler::Nats::new(clients);
+    let http_server = {
+        let setting = setting.clone();
 
-        App::new()
-            .service(nats_handler.register(web::scope("/api")))
-            .service(handler::Healthz::register(web::scope("/healthz")))
-            .service(fs::Files::new("/", "web/dist/cheshmhayash/").index_file("index.html"))
-            .default_service(
-                web::resource("").route(web::get().to(index)).route(
-                    web::route()
-                        .guard(guard::Not(guard::Get()))
-                        .to(HttpResponse::MethodNotAllowed),
-                ),
-            )
-    })
-    .workers(12)
-    .bind(format!(
-        "{}:{}",
-        setting.server().host(),
-        setting.server().port()
-    ))
-    .expect("http server failed to bind")
-    .run()
-    .await
-    .expect("http server failed to run");
+        HttpServer::new(move || {
+            let clients: HashMap<String, nats::Client> = setting
+                .nats()
+                .iter()
+                .map(|s| (s.name().to_string(), nats::Client::new(s.monitoring())))
+                .collect();
+
+            let nats_handler = handler::Nats::new(clients);
+
+            App::new()
+                .service(nats_handler.register(web::scope("/api")))
+                .service(handler::Healthz::register(web::scope("/healthz")))
+                .service(fs::Files::new("/", "web/dist/cheshmhayash/").index_file("index.html"))
+                .default_service(
+                    web::resource("").route(web::get().to(index)).route(
+                        web::route()
+                            .guard(guard::Not(guard::Get()))
+                            .to(HttpResponse::MethodNotAllowed),
+                    ),
+                )
+        })
+    };
+
+    http_server
+        .workers(12)
+        .bind(format!(
+            "{}:{}",
+            setting.server().host(),
+            setting.server().port()
+        ))
+        .expect("http server failed to bind")
+        .run()
+        .await
+        .expect("http server failed to run");
 }
